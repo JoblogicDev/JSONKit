9ea59338490d03c1aaf0fab7a4332a3208b96ad2
Duy Bui <DuyB@joblogic.com>
2025-04-10 10:35:53 +0000

    Merged PR 72101: feat: JOB-56740 - [SPM] - Migrate some libraries to support SPM
    feat: Support SPM

---
+++ Sources/JSONKit/include/NSString+JSONKit.h
@@ -0,0 +1,16 @@
+#ifndef _NSString_JSONKit_h_
+#define _NSString_JSONKit_h_
+
+#import <Foundation/Foundation.h>
+#import "JSONKit.h"
+
+typedef NSUInteger JKParseOptionFlags;
+
+@interface NSString (JSONKit)
+
+- (id)objectFromJSONStringWithParseOptions:(JKParseOptionFlags)parseOptionFlags;
+- (id)objectFromJSONStringWithParseOptions:(JKParseOptionFlags)parseOptionFlags error:(NSError **)error;
+
+@end
+
+#endif // _NSString_JSONKit_h_ 
